d5b52824deca9911f5bf7acc4a159e8c56aaa7f8
Julian Traversa <unitedterrorhots@gmail.com>
2022-06-07 02:41:12 -0400

    ERC-5095 implementation

---
+++ ERC5095.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.4;
+
+import "./ERC/ERC20Permit.sol";
+import "./Interfaces/IAdapter.sol"; 
+import "./Interfaces/IERC5095.sol";  
+import "./Interfaces/IRedeemer.sol";
+
+abstract contract ERC5095 is ERC20Permit, IERC5095 {
+
+    uint256 public override immutable maturity;
+    address public override immutable underlying;
+    uint256 public override maturityRate;
+    IRedeemer public immutable redeemer;
+    address public cToken;
+    IAdapter public adapter;
+
+    error Maturity(uint256 maturity);  
+
+    constructor(address u, uint256 m, address a, address mp) {
+        underlying = u;
+        maturity = m;
+        adapter = IAdapter(a);
+        redeemer = IRedeemer(mp);
+    }
+
+    function convertToUnderlying(uint256 principalAmount) external override view returns (uint256){
+        if (block.timestamp < maturity) {
+            return 0;
+        }
+        return (principalAmount * (adapter.exchangeRateCurrent(cToken) / maturityRate));
+    }
+
+    function convertToPrincipal(uint256 underlyingAmount) external override view returns (uint256){
+        if (block.timestamp < maturity) {
+            return 0;
+        }
+        return(underlyingAmount * maturityRate / adapter.exchangeRateCurrent(cToken));
+    }
+
+    function maxRedeem(address owner) external override view returns (uint256){
+        if (block.timestamp < maturity) {
+            return 0;
+        }
+        return(_balanceOf[owner]);
+    }
+
+    function previewRedeem(uint256 principalAmount) external override view returns (uint256){
+        if (block.timestamp < maturity) {
+            return 0;
+        }
+        return(principalAmount * (adapter.exchangeRateCurrent(cToken) / maturityRate));
+    }
+
+    function maxWithdraw(address owner) external override view returns (uint256){
+        if (block.timestamp < maturity) {
+            return 0;
+        }
+        return(_balanceOf[owner] * (adapter.exchangeRateCurrent(cToken) / maturityRate));
+    }
+
+    function previewWithdraw(uint256 underlyingAmount) external override view returns (uint256){
+        if (block.timestamp < maturity) {
+            return 0;
+        }
+        return(underlyingAmount * maturityRate / adapter.exchangeRateCurrent(cToken));
+    }
+
+    function withdraw(uint256 underlyingAmount, address owner, address receiver) external override returns (uint256){
+        if (maturityRate == 0) {
+            if (block.timestamp < maturity) {
+                revert Maturity(maturity);
+            }
+            maturityRate = adapter.exchangeRateCurrent(cToken);
+            return redeemer.adminRedeem(underlying, maturity, owner, receiver, underlyingAmount);
+        }
+        return redeemer.adminRedeem(underlying, maturity, owner, receiver, (underlyingAmount * maturityRate / adapter.exchangeRateCurrent(cToken)));
+    }
+
+    function redeem(uint256 principalAmount, address owner, address receiver) external override returns (uint256){
+        if (maturityRate == 0) {
+            if (block.timestamp < maturity) {
+                revert Maturity(maturity);
+            }
+            maturityRate = adapter.exchangeRateCurrent(cToken);
+        }
+        return redeemer.adminRedeem(underlying, maturity, owner, receiver, principalAmount);
+    }
+}
